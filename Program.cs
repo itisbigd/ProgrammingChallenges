6ab1aafdc39598fb4ef0a9b1439eb9e5c9d9e7dc
itisbigd <30275561+itisbigd@users.noreply.github.com>
2022-04-07 20:23:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
