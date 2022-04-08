beb1c77dad28406ceefae3342e67a7391f87420b
itisbigd <30275561+itisbigd@users.noreply.github.com>
2022-04-07 21:48:51 -0400

    Solved SortArrayByParity on Leetcode

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ProgrammingChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ SortArrayByParitySolution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingChallenges
+{
+    /*  https://leetcode.com/problems/sort-array-by-parity/
+     *  
+     *  Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+     */
+    public static class SortArrayByParitySolution
+    {
+
+        public static int[] SortArrayByParity(int[] nums)
+        {
+            int numberOfEvens = 0;
+            int numberOfOdds = 0;
+            int[] returnArray = new int[nums.Length];
+
+            foreach (int num in nums)
+            {
+                if (num % 2 == 0)
+                {
+                    returnArray[numberOfEvens] = num;
+                    numberOfEvens++;
+                }
+                else
+                {
+                    returnArray[nums.Length - numberOfOdds] = num;
+                    numberOfOdds++;
+                }
+            }
+
+            return returnArray;
+        }
+        //public static int[] SortArrayByParity(int[] nums)
+        //{
+        //    //O(2n)
+        //    LinkedList<int> linkedList = new LinkedList<int>();
+
+        //    foreach (int num in nums)
+        //    {
+        //        if(num % 2 == 0)
+        //        {
+        //            linkedList.AddFirst(new LinkedListNode<int>(num));
+        //        }
+        //        else
+        //        {
+        //            linkedList.AddLast(new LinkedListNode<int>(num));
+        //        }
+        //    }
+
+        //    linkedList.CopyTo(nums, 0);
+
+        //    return nums;
+        //}
+    }
+}
