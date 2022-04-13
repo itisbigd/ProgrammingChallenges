d077ff5d1c83a092d2971d5c06cac48e294d2c89
itisbigd <30275561+itisbigd@users.noreply.github.com>
2022-04-12 21:22:30 -0400

    Completed Counting Words With a Given Prefix

---
+++ Solutions/Easy/CountingWordsWithAGivenPrefix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingChallenges.Solutions.Easy
+{
+    /* https://leetcode.com/problems/counting-words-with-a-given-prefix/
+     *  
+     *  You are given an array of strings words and a string pref.
+
+        Return the number of strings in words that contain pref as a prefix.
+
+        A prefix of a string s is any leading contiguous substring of s.
+     */
+    public static class CountingWordsWithAGivenPrefix
+    {
+        public static int PrefixCount(string[] words, string pref)
+        {
+            int numberOfWords = 0;
+
+            foreach(string word in words)
+            {
+                if (word.StartsWith(pref))
+                {
+                    numberOfWords++;
+                }
+            }
+
+            return numberOfWords;
+        }
+    }
+}
