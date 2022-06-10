f9c5cd8868773e980c931d69b8e3666fd6d6016d
itisbigd <30275561+itisbigd@users.noreply.github.com>
2022-06-09 22:15:05 -0400

    Tried some more

---
+++ Solutions/Easy/MergeTwoSortedListsSolution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammingChallenges.Solutions.Easy
+{
+    public static class MergeTwoSortedListsSolution
+    {
+
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            //insertion sort
+            while (list2 != null)
+            {
+                bool placed = false;
+                ListNode currentNode = list1;
+                while (!placed)
+                {
+                    if (currentNode.val <= list2.val && currentNode.next.val > list2.val)
+                    {
+
+                    }
+                }
+            }
+            //while loop
+            //minheap
+        }
+
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
